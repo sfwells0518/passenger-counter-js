74928588cc3b5418764e106c53c55790856d5380
Sam Wells <sfwells0518@gmail.com>
2023-07-05 17:27:44 -0500

    add onClick to increment-btn and create increment function in index.js file with console log to test the button

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-document.getElementById("count-el").innerText = 4;
+// document.getElementById("count-el").innerText = 5;
+
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+function increment() {
+  console.log("The button was clicked");
+}
+
+
