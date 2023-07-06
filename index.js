02516a126d3028994c1966856e1e96bb39eb2158
Sam Wells <sfwells0518@gmail.com>
2023-07-06 16:18:07 -0500

    display the count

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+// The DOM = Document Object Model -- aka how you use JS to modify a website //
+
 // document.getElementById("count-el").innerText = 5;
 
+// STEPS NEEDED HERE: //
 // initialize the count as 0
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
-function increment() {
-  console.log("The button was clicked");
-}
+// camelCase
+let countEl = document.getElementById("count-el"); // pass in arguments
 
+// test below via console.log to see if countEl is working properly //
+// console.log(countEl);
+
+let count = 0;
+
+function increment() {
+  count = count + 1;
+  countEl.innerText = count;
+}
 
