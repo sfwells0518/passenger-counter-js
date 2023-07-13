5e71298285c31e7fbef6e9a81792fd9fe31a14df
Sam Wells <sfwells0518@gmail.com>
2023-07-13 12:17:56 -0500

    render the variable in the saveEl using innerText initially, but then switch to textContent for better styling

--- index.js
+++ index.js
@@ -9,16 +9,28 @@
 // change the count-el in the HTML to reflect the new count
 
 // camelCase
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el"); // pass in arguments
 let count = 0;
 
 function increment() {
-  count = count + 1;
-  countEl.innerText = count;
+  count += 1;
+  countEl.textContent = count;
 }
 
 // 1. Create a function, save(), which logs out the count when it's called
-
 function save() {
+// 2. Create a variable that contains both the count and the dash separator, i.e. "12 - 
+  let countStr = count + " - ";
+  // 3. Render the variable in the saveEl using innerText
+  saveEl.textContent += countStr;
+  // NB: Make sure to not delete the existing content of the paragraph
   console.log(count);
 }
+
+// Google: innerText alternative mdn
+
+
+
+
